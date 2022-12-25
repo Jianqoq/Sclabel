89816cecbfcac3ce538d2437bd1f91b78c64ef3b
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2022-12-25 17:58:56 -0500

    Update cap_pic_show.py

--- cap_pic_show.py
+++ cap_pic_show.py
@@ -8,38 +8,36 @@ from functions.Image_process import *
 
 
 class MyLabel2(QLabel):
-    def __init__(self, filename, temploc, temp, save, name,
-                 posx, posy, window, factor, new_width,
-                 new_high, half_width, half_height, main_win,
-                 quality, path, parent=None):
+    def __init__(self, filename, temploc, temp, img_width,
+                 img_height, half_width, half_height, main_win,
+                 quality, parent=None):
         super(MyLabel2, self).__init__(parent=parent)
+        self.mainwindow = main_win
+        self.factor1 = self.mainwindow.dpi
+        self.name2 = self.mainwindow.name
+        self.save = self.mainwindow.readpath
         self.x0 = 0
         self.y0 = 0
-        self.x1 = int(posx)
-        self.y1 = int(posy)
-        self.path = path
+        self.x1 = int(self.mainwindow.width)
+        self.y1 = int(self.mainwindow.height)
+        self.path = self.mainwindow.path
         self.pixmap = None
         self.name = None
         self.tempfile = None
         self.temploc = temploc
         self.temp = temp
-        self.new_width = new_width
-        self.new_high = new_high
+        self.new_width = int(img_width - self.x1 // self.factor1)
+        self.new_high = int(img_height - self.y1 // self.factor1)
         self.half_width = half_width
         self.half_height = half_height
         self.flag = False
         self.quality = quality
         self.filename = filename
-        self.factor1 = factor
-        self.name2 = name
-        self.save = save
         self.count = 0
         self.check = False
         self.nparray = None
         self.toggle = False
         self.pressed = False
-        self.window = window
-        self.mainwindow = main_win
         self.imagedict = None
         self.start = False
         self.index = self.getformat()
@@ -99,7 +97,7 @@ class MyLabel2(QLabel):
                 os.makedirs(self.save)
             cv2.imwrite(path, self.nparray, [self.imagedict[self.index][0], self.imagedict[self.index][2]])
             if self.mainwindow is None:
-                self.window.close()
+                self.parent().close()
             self.dolabelling(path)
         elif event.key() == Qt.Key_Escape and not self.toggle:
             if self.check:
@@ -107,11 +105,7 @@ class MyLabel2(QLabel):
                 self.temp.cleanup()
                 os.unlink(self.name)
             self.toggle = True
-            if self.mainwindow is None:
-                self.window.close()
-            else:
-                self.mainwindow.show()
-                self.window.close()
+            self.parent().close() if self.mainwindow is None else (self.mainwindow.show(), self.parent().close())
 
     def mouseMoveEvent(self, event):
         if not self.check:
@@ -141,11 +135,11 @@ class MyLabel2(QLabel):
 
     def dolabelling(self, path):
         if self.mainwindow.win.checkBox_2.isChecked():
-            self.window.close()
+            self.parent().close()
             self.mainwindow.directannotate(path)
             self.mainwindow.labelling = True
         else:
-            self.window.close()
+            self.parent().close()
             self.mainwindow.show()
 
 
