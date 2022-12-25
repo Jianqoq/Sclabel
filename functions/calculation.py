6a81f96cd4e3a6ed2308de683fb6c6a4a1a13dd5
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2022-12-25 16:48:28 -0500

    Update calculation.py

--- functions/calculation.py
+++ functions/calculation.py
@@ -4,12 +4,14 @@ from os import *
 from math import *
 
 
+# calculate real pos based on High DPI scaling factor
 def label2_event_pos(x, y, factor):
     x = int(x // factor)
     y = int(y // factor)
     return x, y
 
 
+# calculate new_width after rotation
 def new_width(angle, height, width):
     h = height
     w = width
@@ -17,12 +19,12 @@ def new_width(angle, height, width):
     x = 0
     q = 0
     if 0 <= angle <= 90 or 180 <= angle <= 270:
-        x = cos(radians(angle1))  # 0
-        q = cos(radians(90 - angle1))  # 1
+        x = cos(radians(angle1))
+        q = cos(radians(90 - angle1))
     elif 90 < angle < 180 or 270 < angle <= 360:
         angle1 = 180 - angle1
-        x = cos(radians(angle1))  # 0
-        q = cos(radians(90 - angle1))  # 1
+        x = cos(radians(angle1))
+        q = cos(radians(90 - angle1))
     if isclose(x, 0):
         x = 0
     if isclose(q, 0):
@@ -30,6 +32,7 @@ def new_width(angle, height, width):
     return round(abs(h * x + w * q), 1)
 
 
+# calculate new_height after rotation
 def new_height(angle, height, width):
     h = height
     w = width
@@ -62,7 +65,7 @@ def compare(num1, num2, height, width):
     if num1 == num2:
         return height/num1
 
-
+# this function is calculating new matrix for affine_transformation in the Image_processing file
 def generate(rows, cols):
     choose = [0, 1]
     choice = random.choice(choose)
@@ -94,6 +97,7 @@ def generate(rows, cols):
                 return pts1, pts2
 
 
+# increase one or append a number after the filename if the filename is already exists
 def get_validname(savefiledir, name, count, form):
     save = f'{savefiledir}/{name}{str(count)}{form[1]}'
     while path.exists(save):
@@ -101,7 +105,7 @@ def get_validname(savefiledir, name, count, form):
         save = f'{savefiledir}/{name}{str(count)}{form[1]}'
     return save
 
-
+# randomly do image augmentation and display progressbar
 def generate_augmentfile(self, *args):
     images, filedir, progress, count, progressbar, savefiledir, name, format, display, quality = args
     if not path.exists(savefiledir):
