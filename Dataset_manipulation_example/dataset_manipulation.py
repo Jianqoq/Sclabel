a589afc6e6923f531f9f7307bfe30e01a314b78f
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2022-12-28 01:16:23 -0500

    Update dataset_manipulation.py

--- Dataset_manipulation_example/dataset_manipulation.py
+++ Dataset_manipulation_example/dataset_manipulation.py
@@ -16,15 +16,17 @@ def print_result(current, total):
     if index > 0:
         for i in range(index-2):
             lis[i+1] = u'\u25A0'
-            string = ''.join(lis)
-            print(f'\r{string} {percentage}%', end='', flush=True)
+        string = ''.join(lis)
+        print(f'\r{string} {percentage}%', end='', flush=True)
     else:
         string = ''.join(lis)
         print(f'\r{string} {percentage}%', end='', flush=True)
 
+# print_result(40, 100)
+
 
 def mirror(Savelocation, Image_dir):
-    open_dir(Savelocation)
+    open_dir(Savelocation, False)
     directory = os.listdir(Image_dir)
     length = len(directory)
     for index, file in enumerate(directory):
@@ -35,7 +37,7 @@ def mirror(Savelocation, Image_dir):
 
 
 def flip(Savelocation, Image_dir):
-    open_dir(Savelocation)
+    open_dir(Savelocation, False)
     directory = os.listdir(Image_dir)
     length = len(directory)
     for index, file in enumerate(directory):
@@ -45,41 +47,34 @@ def flip(Savelocation, Image_dir):
         print_result(index+1, length)
 
 
-dictionary = {
-    'DB': 1,
-    'DT': 0
-}
-
-
 # Convertion
-def convert(dic):
+def convert(dic, classdict):
     width = int(dic['Image_width'])
     height = int(dic['Image_height'])
     points = ((item['Init Pos'], item['final Pos'], item['Name']) for item in dic['Label'])
-    string = ''
+    lis = []
     for initpt, finalpt, name in points:
-        print(initpt, finalpt, name)
         initpoint = initpt
         finalpoint = finalpt
-        initx = int(initpoint[0])
-        inity = int(initpoint[1])
-        finalx = int(finalpoint[0])
-        finaly = int(finalpoint[1])
+        initx = float(initpoint[0])
+        inity = float(initpoint[1])
+        finalx = float(finalpoint[0])
+        finaly = float(finalpoint[1])
         label_width = abs(finalx - initx)
         label_height = abs(finaly - inity)
         center_x = format(abs(((finalx + initx)/2)/width), '.6f')
         center_y = format(abs(((finaly + inity) / 2) / height), '.6f')
         relative_width = format(label_width/width, '.6f')
         relative_height = format(label_height/height, '.6f')
-        string = string + f'{dictionary[name]} {center_x} {center_y} {relative_width} {relative_height}\n'
-    return string
+        lis.append(f'{classdict[name]} {center_x} {center_y} {relative_width} {relative_height}\n')
+    return ''.join(lis)
 
 
-def convert_mirror(dic):
+def convert_mirror(dic, classdict):
     width = int(dic['Image_width'])
     height = int(dic['Image_height'])
     points = ((item['Init Pos'], item['final Pos'], item['Name']) for item in dic['Label'])
-    string = ''
+    lis = []
     for initpt, finalpt, name in points:
         initpoint = initpt
         finalpoint = finalpt
@@ -93,14 +88,15 @@ def convert_mirror(dic):
         center_y = format(abs(((finaly + inity) / 2) / height), '.6f')
         relative_width = format(label_width/width, '.6f')
         relative_height = format(label_height/height, '.6f')
-        string = string + f'{dictionary[name]} {center_x} {center_y} {relative_width} {relative_height}\n'
-    return string
+        lis.append(f'{classdict[name]} {center_x} {center_y} {relative_width} {relative_height}\n')
+    return ''.join(lis)
 
-def convert_flip(dic):
+
+def convert_flip(dic, classdict):
     width = int(dic['Image_width'])
     height = int(dic['Image_height'])
     points = ((item['Init Pos'], item['final Pos'], item['Name']) for item in dic['Label'])
-    string = ''
+    lis = []
     for initpt, finalpt, name in points:
         initpoint = initpt
         finalpoint = finalpt
@@ -114,14 +110,14 @@ def convert_flip(dic):
         center_y = format(abs(((finaly + inity) / 2) / height-1), '.6f')
         relative_width = format(label_width/width, '.6f')
         relative_height = format(label_height/height, '.6f')
-        string = string + f'{dictionary[name]} {center_x} {center_y} {relative_width} {relative_height}\n'
-    return string
+        lis.append(f'{classdict[name]} {center_x} {center_y} {relative_width} {relative_height}\n')
+    return ''.join(lis)
 
-def convert_flipmirror(dic):
+def convert_flipmirror(dic, classdict):
     width = int(dic['Image_width'])
     height = int(dic['Image_height'])
     points = ((item['Init Pos'], item['final Pos'], item['Name']) for item in dic['Label'])
-    string = ''
+    lis = []
     for initpt, finalpt, name in points:
         initpoint = initpt
         finalpoint = finalpt
@@ -135,11 +131,11 @@ def convert_flipmirror(dic):
         center_y = format(abs(((finaly + inity) / 2) / height-1), '.6f')
         relative_width = format(label_width/width, '.6f')
         relative_height = format(label_height/height, '.6f')
-        string = string + f'{dictionary[name]} {center_x} {center_y} {relative_width} {relative_height}\n'
-    return string
+        lis.append(f'{classdict[name]} {center_x} {center_y} {relative_width} {relative_height}\n')
+    return ''.join(lis)
 
 # Convert Json file to yolov5 format labeling txt file
-def convert_json2label(path0, savepath):
+def convert_json2label(path0, savepath, classdict):
     directory = os.scandir(path0)
     for file in directory:
         if file.name.endswith('.json'):
@@ -147,59 +143,55 @@ def convert_json2label(path0, savepath):
             basename = os.path.splitext(os.path.basename(file_path))[0]
             with open(file_path) as fp:
                 q = json.load(fp)
-                string = convert(q)
                 with open(rf'{savepath}\{basename}.txt', mode='w') as fp2:
-                    fp2.write(string)
+                    fp2.write(convert(q, classdict))
 
-
-def conver_json2txt_mirror(path, savepath):
+def conver_json2txt_mirror(path, savepath, classdict):
     directory = os.listdir(path)
     length = len(directory)
-    open_dir(savepath)
+    open_dir(savepath, False)
     for index, file in enumerate(directory):
         if file.endswith('.json'):
             file_path = os.path.join(path, file)
             basename = os.path.splitext(os.path.basename(file_path))[0]
             with open(file_path) as fp:
                 q = json.load(fp)
-                string = convert_mirror(q)
+                string = convert_mirror(q, classdict)
                 with open(rf'{savepath}\{basename}.txt', mode='a') as fp2:
                     fp2.write(string)
         print_result(index+1, length)
 
 
-def conver_json2txt_flip(path, savepath):
+def conver_json2txt_flip(path, savepath, classdict):
     directory = os.listdir(path)
     length = len(directory)
-    open_dir(savepath)
+    open_dir(savepath, False)
     for index, file in enumerate(directory):
         if file.endswith('.json'):
             file_path = os.path.join(path, file)
             basename = os.path.splitext(os.path.basename(file_path))[0]
             with open(file_path) as fp:
                 q = json.load(fp)
-                string = convert_flip(q)
+                string = convert_flip(q, classdict)
                 with open(rf'{savepath}\{basename}.txt', mode='a') as fp2:
                     fp2.write(string)
         print_result(index+1, length)
 
-def conver_json2txt_flipmirror(path, savepath):
+def conver_json2txt_flipmirror(path, savepath, classdict):
     directory = os.listdir(path)
     length = len(directory)
-    open_dir(savepath)
+    open_dir(savepath, False)
     for index, file in enumerate(directory):
         if file.endswith('.json'):
             file_path = os.path.join(path, file)
             basename = os.path.splitext(os.path.basename(file_path))[0]
             with open(file_path) as fp:
                 q = json.load(fp)
-                string = convert_flipmirror(q)
+                string = convert_flipmirror(q, classdict)
                 with open(rf'{savepath}\{basename}.txt', mode='w+') as fp2:
                     fp2.write(string)
         print_result(index+1, length)
-
-
-#conver_json2txt_flipmirror(r'C:\Users\Public\Pictures\json', r'C:\Users\Public\Pictures\flip_mirror')
+        
 
 TRAIN_DATA_RATIO = 0.2
 origi_dir = r'C:\Users\Public\Pictures\flip'
@@ -209,12 +201,11 @@ suffix = '.txt'
 
 def split_dataset_2_train_val(ratio: float, origi_dir: str, dist_trainimg_dir: str, dist_trainlabel_dir: str,
                               dist_valimg_dir: str, dist_vallabel_dir: str, image_suffix: str, label_suffix: str):
-    real_length = set([file.name for file in os.scandir(origi_dir) if file.name.endswith(image_suffix)])
+    real_length = [file.name for file in os.scandir(origi_dir) if file.name.endswith(image_suffix)]
     length = len(real_length)
-    limit = (1-ratio)*length
-    count = 0
-    for name in real_length:
-        if count <= limit:
+    limit = ratio*length
+    for index, name in enumerate(real_length):
+        if index <= limit:
             basename = os.path.splitext(name)[0]
             newname = f'{basename}{label_suffix}'
             label_file_path = os.path.join(origi_dir, newname)
@@ -225,12 +216,11 @@ def split_dataset_2_train_val(ratio: float, origi_dir: str, dist_trainimg_dir: s
             except Exception as e:
                 print(e)
                 break
-            print_result(count, limit)
-            count += 1
+            print_result(index, limit)
         else:
             pass
     real_length2 = [file.name for file in os.scandir(origi_dir) if file.name.endswith(image_suffix)]
-    for name in real_length2:
+    for index, name in enumerate(real_length2):
         basename = os.path.splitext(name)[0]
         newname = f'{basename}{label_suffix}'
         label_file_path = os.path.join(origi_dir, newname)
@@ -241,14 +231,14 @@ def split_dataset_2_train_val(ratio: float, origi_dir: str, dist_trainimg_dir: s
         except Exception as e:
             print(e)
             break
-        print_result(count, len(real_length2))
+        print_result(index, len(real_length2))
     else:
         pass
     print('\nFile Transfer Successfully!')
 
 
 def rename(dir, dst, name, suffix, start):
-    open_dir(dir)
+    open_dir(dir, False)
     file_list = os.listdir(dir)
     length = len(file_list)
     count = 0
@@ -263,14 +253,4 @@ def rename(dir, dst, name, suffix, start):
             count += 1
         print_result(index+1, length)
 
-
-#rename(r'C:\Users\Public\Pictures\flip_mirror', r'C:\Users\Public\Pictures\data\lABEL', 'Image', '.txt', 1883)
-
-
-split_dataset_2_train_val(0.8, r'C:\Users\Public\Pictures\data\DATASET',
-                          r'C:\Users\Public\Pictures\VOCdevkit\images\Val',
-                          r'C:\Users\Public\Pictures\VOCdevkit\labels\Val',
-                          r'C:\Users\Public\Pictures\VOCdevkit\images\Train',
-                          r'C:\Users\Public\Pictures\VOCdevkit\labels\Train',
-                          '.png', '.txt')
 
