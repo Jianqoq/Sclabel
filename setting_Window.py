0f2c59e4208ee39059d51d64267a9fe71190fbf8
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2022-12-29 15:19:00 -0500

    Update setting_Window.py

--- setting_Window.py
+++ setting_Window.py
@@ -9,42 +9,49 @@ class SettingWindow(QWidget):
     def __init__(self, main_win_pos, size, main_win):
         super(SettingWindow, self).__init__()
         self.win = UI.setting.Ui_Form()
+        win = self.win
+        win.setupUi(self)
         self.setObjectName('Settings')
-        self.ui = self.win.setupUi(self)
         self.setWindowModality(Qt.ApplicationModal)
         self.setGeometry(QRect(0, 0, 0, 0))
         self.mainwin = main_win
         self.configname = self.mainwin.configname
         self.path = rf'C:\Users\{os.getlogin()}\Documents\Sclabel'
         self.init_text()
-        self.win.progressBar.hide()
-        self.win.pushButton_2.setEnabled(False)
-        self.win.pushButton.clicked.connect(lambda: self.getdir(self.win.lineEdit))
-        self.win.pushButton_2.clicked.connect(self.confirm)
-        self.win.pushButton_5.clicked.connect(lambda: displayProgressbar(self))
-        self.win.pushButton_3.clicked.connect(self.cancel_button)
-        self.win.pushButton_5.clicked.connect(self.threading1)
-        self.win.pushButton_4.clicked.connect(self.ok_button)
-        self.win.pushButton_6.clicked.connect(lambda: self.getdir(self.win.lineEdit_4))
-        self.win.pushButton_7.clicked.connect(lambda: self.getdir(self.win.lineEdit_6))
-        self.win.pushButton_11.clicked.connect(lambda: self.getdir(self.win.lineEdit_7))
-        self.win.pushButton_8.clicked.connect(lambda: self.openfolder(10))
-        self.win.pushButton_9.clicked.connect(lambda: self.openfolder(9))
-        self.win.pushButton_10.clicked.connect(lambda: self.openfolder(8))
-        self.win.pushButton_12.clicked.connect(lambda: self.openfolder(12))
-        self.win.lineEdit.textChanged.connect(self.enableapply)
-        self.win.lineEdit_2.textEdited.connect(self.enableapply)
-        self.win.lineEdit_3.textEdited.connect(self.enableapply)
-        self.win.lineEdit_4.textChanged.connect(self.enableapply)
-        self.win.lineEdit_6.textChanged.connect(self.enableapply)
-        self.win.lineEdit_7.textChanged.connect(self.enableapply)
-        self.win.horizontalSlider.valueChanged.connect(self.enableapply)
-        self.win.horizontalSlider.valueChanged.connect(self.updateslider)
-        self.win.comboBox_2.currentIndexChanged.connect(self.enableapply)
+        win.progressBar.hide()
+        win.pushButton_2.setEnabled(False)
+        
+        # connect signals
+        win.pushButton.clicked.connect(lambda: self.getdir(win.lineEdit))
+        win.pushButton_2.clicked.connect(self.confirm)
+        win.pushButton_5.clicked.connect(lambda: displayProgressbar(self))
+        win.pushButton_3.clicked.connect(self.cancel_button)
+        win.pushButton_5.clicked.connect(self.threading1)
+        win.pushButton_4.clicked.connect(self.ok_button)
+        win.pushButton_6.clicked.connect(lambda: self.getdir(win.lineEdit_4))
+        win.pushButton_7.clicked.connect(lambda: self.getdir(win.lineEdit_6))
+        win.pushButton_11.clicked.connect(lambda: self.getdir(win.lineEdit_7))
+        win.pushButton_8.clicked.connect(lambda: self.openfolder(10))
+        win.pushButton_9.clicked.connect(lambda: self.openfolder(9))
+        win.pushButton_10.clicked.connect(lambda: self.openfolder(8))
+        win.pushButton_12.clicked.connect(lambda: self.openfolder(12))
+        win.lineEdit.textChanged.connect(self.enableapply)
+        win.lineEdit_2.textEdited.connect(self.enableapply)
+        win.lineEdit_3.textEdited.connect(self.enableapply)
+        win.lineEdit_4.textChanged.connect(self.enableapply)
+        win.lineEdit_6.textChanged.connect(self.enableapply)
+        win.lineEdit_7.textChanged.connect(self.enableapply)
+        win.horizontalSlider.valueChanged.connect(self.enableapply)
+        win.horizontalSlider.valueChanged.connect(self.updateslider)
+        win.comboBox_2.currentIndexChanged.connect(self.enableapply)
+
         self.imgdict = None
-        self.winx = int(main_win_pos.x() + size.width()//2 - self.size().width()//2)
-        self.winy = int(main_win_pos.y() + size.height()//2 - self.size().height()//2)
         self.name = 'image'
+
+        # init setting window display pos
+        self_size = self.size()
+        self.winx = int(main_win_pos.x() + size.width()//2 - self_size.width()//2)
+        self.winy = int(main_win_pos.y() + size.height()//2 - self_size.height()//2)
         self.window_pos()
         self.move(self.winx, self.winy)
 
@@ -74,9 +81,11 @@ class SettingWindow(QWidget):
         self.win.pushButton_2.setEnabled(True)
 
     def dataaugment(self):
-        dataaugment(self.win.lineEdit_6.text(), self.win.lineEdit_4.text(), self.win.lineEdit_3.text(),
-                    self.win.lineEdit_5, self.win.progressBar, self.mainwin.imgdict[self.mainwin.image_format],
-                    self.mainwin.slidervalue)
+        win = self.win
+        mainwin = self.mainwin
+        dataaugment(win.lineEdit_6.text(), win.lineEdit_4.text(), win.lineEdit_3.text(),
+                    win.lineEdit_5, win.progressBar, mainwin.imgdict[mainwin.image_format],
+                    mainwin.slidervalue)
 
     def ok_button(self):
         self.confirm()
@@ -94,30 +103,31 @@ class SettingWindow(QWidget):
         win = self.win
         mainwin = self.mainwin
         self.name = win.lineEdit_2.text()
-        self.dir = win.lineEdit.text()
-        self.dir2 = win.lineEdit_4.text()
-        self.name2 = win.lineEdit_3.text()
-        self.aug_load = win.lineEdit_6.text()
-        self.format = str(win.comboBox_2.currentIndex())
-        self.labellication = win.lineEdit_7.text()
+        dir = win.lineEdit.text()
+        dir2 = win.lineEdit_4.text()
+        name2 = win.lineEdit_3.text()
+        aug_load = win.lineEdit_6.text()
+        format = str(win.comboBox_2.currentIndex())
+        labellication = win.lineEdit_7.text()
         mainwin.slidervalue = win.horizontalSlider.value()
-        mainwin.imageformat = self.format
-        mainwin.readpath = self.dir
+        mainwin.imageformat = format
+        mainwin.readpath = dir
         mainwin.name = self.name
-        mainwin.save = self.dir2
-        mainwin.Aug_name = self.name2
-        mainwin.Aug_load = self.aug_load
+        mainwin.save = dir2
+        mainwin.Aug_name = name2
+        mainwin.Aug_load = aug_load
 
-        if not self.name or not self.dir or not self.dir2 or not self.name2 or not self.aug_load:
+        if not self.name or not dir or not dir2 or not name2 or not aug_load:
             Messagebox('Input name can\'t be empty')
         else:
-            self.updatesettings(self.dir, self.dir2, self.name, self.name2, self.aug_load, self.format,
-                                self.labellication, self.configname)
+            self.updatesettings(dir, dir2, self.name, name2, aug_load, format,
+                                labellication, self.configname)
             win.pushButton_2.setEnabled(False)
 
     def updateslider(self):
-        value = self.win.horizontalSlider.value()
-        self.win.label_9.setText(str(value))
+        win = self.win
+        value = win.horizontalSlider.value()
+        win.label_9.setText(str(value))
 
     def updatesettings(self, dir, dir2, name, name2, aug_load, suffix, dir3, configname):
         path = self.path
