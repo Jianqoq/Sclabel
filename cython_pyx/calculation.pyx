9a15d9c2f3b62b77b229eb2924d9818fcf9e2c33
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2022-12-29 16:52:00 -0500

    Update calculation.pyx

--- cython_pyx/calculation.pyx
+++ cython_pyx/calculation.pyx
@@ -2,7 +2,7 @@ from random import *
 from numpy import *
 from os import *
 from math import *
-
+from functions.Image_process import *
 
 cpdef tuple label2_event_pos(int x, int y, double factor):
     cdef double x2, y2
@@ -11,24 +11,7 @@ cpdef tuple label2_event_pos(int x, int y, double factor):
     return int(x2), int(y2)
 
 
-cpdef double new_width(double angle, int height, int width):
-    cdef double x, q
-
-    if 0 <= angle <= 90 or 180 <= angle <= 270:
-        x = cos(radians(angle1))
-        q = cos(radians(90 - angle1))
-    elif 90 < angle < 180 or 270 < angle <= 360:
-        angle = 180 - angle
-        x = cos(radians(angle))
-        q = cos(radians(90 - angle))
-    if isclose(x, 0):
-        x = 0
-    if isclose(q, 0):
-        q = 0
-    return format(abs(height * x + width * q), '.1f')
-
-
-cdef double new_height(double angle, int height, int width):
+cpdef float new_width(double angle, int height, int width):
     cdef double x, q
     if 0 <= angle <= 90 or 180 <= angle <= 270:
         x = cos(radians(angle))
@@ -41,10 +24,26 @@ cdef double new_height(double angle, int height, int width):
         x = 0
     if isclose(q, 0):
         q = 0
-    return format(abs(height * q + width * x), '.1f')
+    return abs(height * x + width * q)
 
 
-cpdef double compare(int num1, int num2, int height, int width):
+cpdef float new_height(double angle, int height, int width):
+    cdef double x, q
+    if 0 <= angle <= 90 or 180 <= angle <= 270:
+        x = cos(radians(angle))
+        q = cos(radians(90 - angle))
+    elif 90 < angle < 180 or 270 < angle <= 360:
+        angle = 180 - angle
+        x = cos(radians(angle))
+        q = cos(radians(90 - angle))
+    if isclose(x, 0):
+        x = 0
+    if isclose(q, 0):
+        q = 0
+    return abs(height * q + width * x)
+
+
+cpdef float compare(float num1, float num2, int height, int width):
     if num1 > num2:
         if num2 > width:
             return min(width/num2, height/num1)
@@ -90,7 +89,7 @@ def generate(int rows, int cols):
                 return pts1, pts2
 
 
-def get_validname(savefiledir, name, count, form):
+cpdef str get_validname(str savefiledir, str name, int count, form):
     save = f'{savefiledir}/{name}{str(count)}{form[1]}'
     while path.exists(save):
         count += 1
@@ -98,7 +97,9 @@ def get_validname(savefiledir, name, count, form):
     return save
 
 
-def generate_augmentfile(self, *args):
+def generate_augmentfile(*args):
+    cdef int progress, count, quality
+    cdef str loc, name, savefiledir
     images, filedir, progress, count, progressbar, savefiledir, name, format, display, quality = args
     if not path.exists(savefiledir):
         makedirs(savefiledir)
@@ -107,6 +108,7 @@ def generate_augmentfile(self, *args):
         progress += 1
         count += 1
         progressbar.setValue(progress)
+        progressbar.update()
         save = get_validname(savefiledir, name, count, format)
-        foo_list = [self.translation, self.rotation90, self.rotation, self.rotation180]
+        foo_list = [translation, rotation90, rotation, rotation180]
         random.choice(foo_list)(loc, save, display, format[0], quality)
