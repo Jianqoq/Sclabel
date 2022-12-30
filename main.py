052387e074f42b5fdcb98edfdde46e827b9dac42
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2022-12-29 19:54:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -49,33 +49,40 @@ class Win(QMainWindow):
         self.labelling = False
         self.configname = 'config.ini'
         readdir(self, path, os.getlogin())
-        self.onlyint = QIntValidator()
-        win.lineEdit.setValidator(self.onlyint)
-        win.lineEdit_2.setValidator(self.onlyint)
+
+        # initialize var
+        self.init_var(path, win)
+
+        slidervalue = self.slidervalue
+        compression = self.compression = int(round(100/slidervalue, 0))
+        self.imgdict = {
+            '0': (int(cv2.IMWRITE_JPEG_QUALITY), '.jpg', slidervalue),
+            '1': (int(cv2.IMWRITE_PNG_COMPRESSION), '.png', compression),
+            '2': (int(cv2.IMWRITE_TIFF_COMPRESSION), '.tiff', compression),
+            '3': (None, '.bmp', None),
+            '4': (None, '.jp2', None),
+        }
+
+        onlyint = self.onlyint = QIntValidator()
+        win.lineEdit.setValidator(onlyint)
+        win.lineEdit_2.setValidator(onlyint)
+
         win.frame_3.installEventFilter(self)
-        win.settings.clicked.connect(self.settingwin)
-        win.create.clicked.connect(self.cap_opennewwin)
-        win.pushButton_3.clicked.connect(self.annotation)
+        win.settings.clicked.connect(lambda: self.settingwin(self.pos(), win.frame_2))
+        win.create.clicked.connect(lambda: self.cap_opennewwin(win, self.slidervalue))
+        win.pushButton_3.clicked.connect(lambda: self.annotation(win))
         win.checkBox.toggled.connect(lambda: self.update4(path, 'check', win.checkBox.isChecked()))
         win.checkBox_2.toggled.connect(lambda: self.update3(path, 'labeling function', win.checkBox_2.isChecked()))
         win.lineEdit.textEdited.connect(lambda: w_update(self, 'Saving setting', 'width', win.lineEdit))
         win.lineEdit_2.textEdited.connect(lambda: h_update(self, 'Saving setting', 'height', win.lineEdit_2))
-        self.init_var()
+
         self.move(self.posx, self.posy)
-        self.compression = int(round(100/self.slidervalue, 0))
-        self.imgdict = {
-            '0': (int(cv2.IMWRITE_JPEG_QUALITY), '.jpg', self.slidervalue),
-            '1': (int(cv2.IMWRITE_PNG_COMPRESSION), '.png', self.compression),
-            '2': (int(cv2.IMWRITE_TIFF_COMPRESSION), '.tiff', self.compression),
-            '3': (None, '.bmp', None),
-            '4': (None, '.jp2', None),
-        }
+
         self.show()
 
-    def annotation(self):
+    def annotation(self, win):
         self.window2 = QLineEditMask(geo.width(), geo.height(), self)
         window2 = self.window2
-        win = self.win
         window2.show()
         point = win.frame.mapToGlobal(win.frame.pos())
         x = point.x() + win.frame.width()//2 - window2.geometry().width()//2
@@ -100,31 +107,32 @@ class Win(QMainWindow):
         window.label.show()
         window.hide()
 
-    def init_var(self):
+    def init_var(self, path, win):
         config = configparser.ConfigParser()
-        config.read(rf'{self.path}\{self.configname}')
-        self.readpath = config.get('Saving setting', 'Location')
+        config.read(rf'{path}\{self.configname}')
+        readpath = self.readpath = config.get('Saving setting', 'Location')
         self.name = config.get('Saving setting', 'Image name')
-        self.save = config.get('Saving setting', 'Save Location')
+        save = self.save = config.get('Saving setting', 'Save Location')
         self.Aug_name = config.get('Saving setting', 'Augment Image name')
-        self.Aug_load = config.get('Saving setting', 'Augment Data load')
-        self.width = config.get('Saving setting', 'width')
-        self.height = config.get('Saving setting', 'height')
+        Aug_load = self.Aug_load = config.get('Saving setting', 'Augment Data load')
+        width = self.width = config.get('Saving setting', 'width')
+        height = self.height = config.get('Saving setting', 'height')
         self.posx = int(config.get('Saving setting', 'last_posx'))
         self.posy = int(config.get('Saving setting', 'last_posy'))
-        self.check = config.get('Saving setting', 'check')
+        check = self.check = config.get('Saving setting', 'check')
         self.slidervalue = int(config.get('Saving setting', 'Quality'))
         self.image_format = config.get('Saving setting', 'Image Format')
-        self.imglabel = config.get('Saving setting', 'Label Location')
-        self.labelcheckbox = config.get('Saving setting', 'Labeling Function')
-        self.win.lineEdit.setText(self.width)
-        self.win.lineEdit_2.setText(self.height)
-        self.checkTrue(self.check, self.win.checkBox)
-        self.checkTrue(self.labelcheckbox, self.win.checkBox_2)
-        open_dir(self.readpath, False)
-        open_dir(self.save, False)
-        open_dir(self.Aug_load, False)
-        open_dir(self.imglabel, False)
+        imglabel = self.imglabel = config.get('Saving setting', 'Label Location')
+        labelcheckbox = self.labelcheckbox = config.get('Saving setting', 'Labeling Function')
+
+        win.lineEdit.setText(width)
+        win.lineEdit_2.setText(height)
+        self.checkTrue(check, win.checkBox)
+        self.checkTrue(labelcheckbox, win.checkBox_2)
+        open_dir(readpath, False)
+        open_dir(save, False)
+        open_dir(Aug_load, False)
+        open_dir(imglabel, False)
 
     def checkTrue(self, key, checkbox):
         checkbox.setChecked(True) if key == 'True' else checkbox.setChecked(False)
@@ -139,21 +147,21 @@ class Win(QMainWindow):
 
     @QtCore.pyqtSlot(int, int)
     def updatelabelinfo(self, width, height):
-        if self.label is not None:
-            self.label.x1 = width
-            self.label.y1 = height
+        label = self.label
+        if label is not None:
+            label.x1 = width
+            label.y1 = height
         else:
             self.width = width
             self.height = height
 
-    def settingwin(self):
-        frame_2 = self.win.frame_2
-        pos = self.pos() + frame_2.pos()
+    def settingwin(self, pos, frame_2):
+        pos = pos + frame_2.pos()
         self.settingw = SettingWindow(pos, frame_2.size(), self)
-        self.settingw.imgdict = self.imgdict
-        self.settingw.show()
+        settingw = self.settingw
+        settingw.show()
 
-    def cap_opennewwin(self):
+    def cap_opennewwin(self, win, slidervalue):
         self.temp = tempfile.TemporaryDirectory()
         temp = self.temp
         name = temp.name
@@ -161,18 +169,19 @@ class Win(QMainWindow):
         w = MoniterDev[0][2][2]
         h = MoniterDev[0][2][3]
         temploc = rf'{name}/image.jpg'
-        if self.win.checkBox.isChecked():
+        if win.checkBox.isChecked():
             self.hide()
         window_capture(temploc, 0, 0, w, h)
-        self.open_window(temploc, temp, self.slidervalue)
+        self.open_window(temploc, temp, slidervalue, win)
 
     def keyPressEvent(self, event):
-        self.cap_opennewwin() if event.key() == 39 else False
+        self.cap_opennewwin(self.win, self.slidervalue) if event.key() == 39 else False
 
     def eventFilter(self, source, event):
         win = self.win
-        if source == win.frame_3 and event.type() == QtCore.QEvent.MouseMove and QtCore.Qt.LeftButton and self.offset:
-            self.pos2 = self.pos() + event.globalPos() - self.offset
+        offset = self.offset
+        if source == win.frame_3 and event.type() == QtCore.QEvent.MouseMove and QtCore.Qt.LeftButton and offset:
+            self.pos2 = self.pos() + event.globalPos() - offset
             self.move(self.pos2)
             self.offset = event.globalPos()
             event.accept()
@@ -183,7 +192,7 @@ class Win(QMainWindow):
 
         return super().eventFilter(source, event)
 
-    def open_window(self, temploc, temp, quality):
+    def open_window(self, temploc, temp, quality, win):
         factor = self.dpi
         image = QPixmap(temploc)
         image.setDevicePixelRatio(factor)
@@ -195,7 +204,7 @@ class Win(QMainWindow):
         wind.new_win.setupUi(wind)
         newx = int(x // factor)
         newy = int(y // factor)
-        win_instance = self if self.win.checkBox.isChecked() else None
+        win_instance = self if win.checkBox.isChecked() else None
         self.label = MyLabel2(temploc, temp, x, y, newx, newy, win_instance, quality, wind)
         label = self.label
         label.setPixmap(image)
@@ -208,11 +217,13 @@ class Win(QMainWindow):
         wind.show()
 
     def updateidct(self):
-        self.compression = int(round(100/self.slidervalue, 0))
+        slidervalue = self.slidervalue
+        self.compression = int(round(100/slidervalue, 0))
+        compression = self.compression
         self.imgdict = {
-            '0': (int(cv2.IMWRITE_JPEG_QUALITY), '.jpg', self.slidervalue),
-            '1': (int(cv2.IMWRITE_PNG_COMPRESSION), '.png', self.compression),
-            '2': (int(cv2.IMWRITE_TIFF_COMPRESSION), '.tiff', self.compression),
+            '0': (int(cv2.IMWRITE_JPEG_QUALITY), '.jpg', slidervalue),
+            '1': (int(cv2.IMWRITE_PNG_COMPRESSION), '.png', compression),
+            '2': (int(cv2.IMWRITE_TIFF_COMPRESSION), '.tiff', compression),
             '3': (None, '.bmp', None),
             '4': (None, '.jp2', None),
         }
